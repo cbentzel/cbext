7bac2557055bddbdcac52d111316432cd5fe9ed8
Chris Bentzel <cbentzel@chromium.org>
2011-12-02 17:36:10 -0500

    More logging of tabs

--- pretab/background.js
+++ pretab/background.js
@@ -1,10 +1,33 @@
+function logTab(context, tab) {
+  console.log("%s: url=%s id=%s window=%s index=%s", context, tab.url, tab.id, tab.windowId, tab.index);
+}
+
+function getAndShowTab(context, tabId) {
+  chrome.tabs.get(tabId, function (tab) {
+      logTab(context, tab);
+    });
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
       if (details.url.indexOf("://cbentzel-dev-test.appspot.com") != -1) {
-        chrome.tabs.get(details.tabId, function(x) { console.log(x); });
+        getAndShowTab("onBeforeRequest", details.tabId);
       }
     },
     {},
     []
+);
+
+
+chrome.tabs.onCreated.addListener(
+    function (tab) {
+      logTab("created", tab);
+    }
+);
+
+chrome.tabs.onRemoved.addListener(
+    function (tabId, attachInfo) {
+      getAndShowTab("removed", tabId);
+    }
 );
 

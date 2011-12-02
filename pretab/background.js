ba50222edfd21b11bb7b2fab6a45cfe041fe42fa
Chris Bentzel <cbentzel@chromium.org>
2011-12-02 16:09:02 -0500

    Update to use webRequest

--- pretab/background.js
+++ pretab/background.js
@@ -1,1 +1,10 @@
-var x = 2;
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      if (details.url.indexOf("://cbentzel-dev-test.appspot.com") != -1) {
+        chrome.tabs.get(details.tabId, function(x) { console.log(x); });
+      }
+    },
+    {},
+    []
+);
+
